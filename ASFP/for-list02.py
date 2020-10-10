6c1ad9869b51cb5596f48e09f9503e51f71bccc7
sainthm <sainthm@naver.com>
2020-10-11 00:40:18 +0900

    add for advanced example

---
+++ ASFP/for-list02.py
@@ -0,0 +1,11 @@
+name_set = ["드링킹 요구르트", "딸기 우유"]
+price_set = [1800, 1500]
+qty_set = [4, 2]
+
+name = name_set[0]
+sales = price_set[0] * qty_set[0]
+print(name + " 매출액: " + str(sales))
+
+name = name_set[1]
+sales = price_set[1] * qty_set[1]
+print(name + " 매출액: " + str(sales))
