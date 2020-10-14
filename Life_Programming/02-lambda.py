8b2168fb61f54fa085cb573fe9d215499b3e2075
sainthm <sainthm@naver.com>
2020-10-14 10:40:30 +0900

    add lambda example 2

--- Life_Programming/02-lambda.py
+++ Life_Programming/02-lambda.py
@@ -3,3 +3,9 @@ print(y(12))
 
 add = lambda a, b : a + b
 print(add(2, 3))
+
+little_prince = '''여섯 살 적에 나는 '체험한 이야기'라는 제목의, 원시림에 관한 책에서 기막힌 그림 하나를 본 적이 있다.'''
+print(little_prince[:10])
+
+short = lambda x : x[:10]
+print(short(little_prince))
