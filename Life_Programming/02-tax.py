e150dd94e91407e1515fe99f6e0414a71e191af6
sainthm <sainthm@naver.com>
2020-10-14 11:30:49 +0900

    modify tax example

--- Life_Programming/02-tax.py
+++ Life_Programming/02-tax.py
@@ -3,3 +3,31 @@
 # 소비자 가격 = 물건 가격 * 1.1
 # 물건 가격 = 소비자가격 * (1/1.1)
 # 부가세 = 물건 가격 * 0.1 = 소비자 가격 * (1/11)
+
+# 시나리오
+# 세 가지 서비스가 존재
+# a 서비스: 23만원, b 서비스: 40만원, c 서비스: 67만원
+# 부가세 유무에 따른 지불 비용 출력 예제
+
+price = [23, 40, 67]
+
+def service_price():
+    service = input('이용하실 서비스 종류를 입력하세요, a/b/c: ')
+    valueAdded = input('부가세를 포함합니까?, yes/no: ')
+    if valueAdded == 'yes':
+        if service == 'a':
+            result = 23 * 1.1
+        if service == 'b':
+            result = 40 * 1.1
+        if service == 'c':
+            result = 67 * 1.1
+    if valueAdded == 'no':
+        if service == 'a':
+            result = 23
+        if service == 'b':
+            result = 40
+        if service == 'c':
+            result = 67
+    print(round(result, 1), '만 원입니다.')
+
+service_price()
